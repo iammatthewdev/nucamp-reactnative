5be158453311d604879887d64a3a0ab45daf32a4
Matthew Evans <m@iammatthew.com>
2021-09-09 20:04:54 -0700

    Gestures Part 2

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -33,42 +33,49 @@ const mapDispatchToProps = {
 function RenderCampsite(props) {
   const { campsite } = props;
 
-  const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
+  const view = React.createRef();
 
-    const panResponder = PanResponder.create({
-        onStartShouldSetPanResponder: () => true,
-        onPanResponderEnd: (e, gestureState) => {
-            console.log('pan responder end', gestureState);
-            if (recognizeDrag(gestureState)) {
-                Alert.alert(
-                    'Add Favorite',
-                    'Are you sure you wish to add ' + campsite.name + ' to favorites?',
-                    [
-                        {
-                            text: 'Cancel',
-                            style: 'cancel',
-                            onPress: () => console.log('Cancel Pressed')
-                        },
-                        {
-                            text: 'OK',
-                            onPress: () => props.favorite ?
-                                console.log('Already set as a favorite') : props.markFavorite()
-                        }
-                    ],
-                    { cancelable: false }
-                );
+  const recognizeDrag = ({ dx }) => (dx < -200) ? true : false;
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderGrant: () => {
+      view.current.rubberBand(1000)
+        .then(endState => console.log(endState.finished ? 'finished' : 'canceled'));
+    },
+    onPanResponderEnd: (e, gestureState) => {
+      console.log('pan responder end', gestureState);
+      if (recognizeDrag(gestureState)) {
+        Alert.alert(
+          'Add Favorite',
+          'Are you sure you wish to add ' + campsite.name + ' to favorites?',
+          [
+            {
+              text: 'Cancel',
+              style: 'cancel',
+              onPress: () => console.log('Cancel Pressed')
+            },
+            {
+              text: 'OK',
+              onPress: () => props.favorite ?
+                console.log('Already set as a favorite') : props.markFavorite()
             }
-            return true;
-        }
-    });
+          ],
+          { cancelable: false }
+        );
+      }
+      return true;
+    }
+  });
 
   if (campsite) {
     return (
       <Animatable.View
-                animation='fadeInDown'
-                duration={2000}
-                delay={1000}
-                {...panResponder.panHandlers}>
+        animation='fadeInDown'
+        duration={2000}
+        delay={1000}
+        ref={view}
+        {...panResponder.panHandlers}>
         <Card
           featuredTitle={campsite.name}
           image={{ uri: baseUrl + campsite.image }}
